abc6507b6620077dbd4d4e145174dd1077a222cb
VeejaLiu <veejaliu@gmail.com>
2024-08-09 17:08:16 +0800

    change the style of PlayerTrendsPage.tsx

--- src/pages/PlayerTrendsPage.tsx
+++ src/pages/PlayerTrendsPage.tsx
@@ -1,16 +1,62 @@
 import * as React from 'react';
-import { Area, AreaChart, CartesianGrid, Tooltip, YAxis } from 'recharts';
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import { PlayerApis, PlayerTrendData } from '../service/PlayerApis.ts';
 import { useEffect } from 'react';
 import { getAvatarUrl, getColorByPositionType } from './PlayerListPage.tsx';
 import { Popover, Space } from '@douyinfe/semi-ui';
 import './PlayerTrendsPage.css';
 
+function formatDate(inputDate: string) {
+  const [, month, day] = inputDate.split('-').map(Number);
+
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const monthName = monthNames[month - 1];
+
+  return `${monthName} ${day}`;
+
+  // const suffix =
+  //   day % 10 === 1 && day !== 11
+  //     ? 'st'
+  //     : day % 10 === 2 && day !== 12
+  //       ? 'nd'
+  //       : day % 10 === 3 && day !== 13
+  //         ? 'rd'
+  //         : 'th';
+  //
+  // return `${monthName} ${day}${suffix}`;
+}
+
 function PlayerTrendsPage(): React.ReactElement {
   const [data, setData] = React.useState<PlayerTrendData[]>([]);
 
   const fetchPlayerTrends = async () => {
     const res = await PlayerApis.getPlayerTrends();
+    res.forEach((player) => {
+      player.trends.forEach((trend) => {
+        trend.inGameDate = formatDate(trend.inGameDate);
+      });
+    });
     setData(res);
   };
 
@@ -62,14 +108,22 @@ function PlayerTrendsPage(): React.ReactElement {
             <Space wrap align={'start'}>
               {data
                 .filter((player) => player.positionType === item.position)
+                .sort((a, b) => {
+                  return (
+                    Math.max(...b.trends.map((item) => item.potential)) -
+                    Math.max(...a.trends.map((item) => item.potential))
+                  );
+                })
                 .map((player) => {
                   const minOverallRating =
                     Math.min(
                       ...player.trends.map((item) => item.overallRating),
                     ) - 10;
+                  // const minOverallRating = 40;
                   const maxPotential =
                     Math.max(...player.trends.map((item) => item.potential)) +
-                    5;
+                    3;
+                  // const maxPotential = 100;
                   const positionColor = getColorByPositionType(
                     player.positionType,
                   );
@@ -78,11 +132,10 @@ function PlayerTrendsPage(): React.ReactElement {
                     <Space
                       key={player.playerID}
                       style={{
-                        border: '1px solid gray',
-                        borderRadius: '5px',
+                        // border: '1px solid gray',
+                        borderRadius: '3px',
+                        backgroundColor: '#f1f1f1',
                         padding: '10px',
-                        height: '200px',
-                        width: '450px',
                       }}
                     >
                       <Space
@@ -90,15 +143,18 @@ function PlayerTrendsPage(): React.ReactElement {
                         spacing={0}
                         style={{
                           backgroundColor: '#e4ce78',
-                          borderRadius: '5px',
-                          boxShadow: '0 0 5px #000',
+                          borderRadius: '3px',
+                          // boxShadow: '0 0 5px #000',
                           width: '120px',
-                          height: '200px',
+                          height: '170px',
                         }}
                       >
-                        <div style={{ width: 120, height: 120 }}>
+                        <span style={{ color: positionColor }}>
+                          <h4>{player.preferredposition1}</h4>
+                        </span>
+                        <div style={{ width: 100, height: 100 }}>
                           <img
-                            style={{ width: 120, height: 120 }}
+                            style={{ width: 100, height: 100 }}
                             src={imageUrl}
                             alt="player"
                             onError={(e) => {
@@ -120,71 +176,68 @@ function PlayerTrendsPage(): React.ReactElement {
                             {player.playerName}
                           </span>{' '}
                         </Popover>
-
-                        <span style={{ color: positionColor }}>
-                          <b>{player.preferredposition1}</b>
-                        </span>
                       </Space>
                       <AreaChart
                         // style={{ backgroundColor: 'yellow' }}
-                        width={330}
-                        height={200}
+                        width={300}
+                        height={170}
                         data={player.trends}
-                        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+                        margin={{ top: 10, right: 30, left: 0 }}
                       >
-                        {/*<XAxis*/}
-                        {/*  dataKey="inGameDate"*/}
-                        {/*  style={{*/}
-                        {/*    fontSize: '10px',*/}
-                        {/*  }}*/}
-                        {/*></XAxis>*/}
+                        <XAxis
+                          dataKey="inGameDate"
+                          type={'category'}
+                          style={{
+                            fontSize: '10px',
+                          }}
+                        ></XAxis>
                         <YAxis
                           domain={[
                             minOverallRating,
                             Math.min(maxPotential, 100),
                           ]}
-                          style={
-                            {
-                              // fontSize: '12px',
-                            }
-                          }
+                          style={{
+                            fontSize: '10px',
+                          }}
                         ></YAxis>
                         <CartesianGrid strokeDasharray="3 3" />
-                        <Tooltip
-                          content={({ active, payload }) => {
-                            if (active && payload && payload.length) {
-                              const { overallRating, potential } =
-                                payload[0].payload; // 获取当前数据点的能力值和潜力值
-                              return (
-                                <div>
-                                  <p>{payload[0].payload.inGameDate}</p>
-                                  <p>
-                                    <b>Potential:</b> {potential}
-                                  </p>
-                                  <p>
-                                    <b>Overall:</b> {overallRating}
-                                  </p>
-                                </div>
-                              );
-                            }
-                            return null; // 不显示 Tooltip
-                          }}
-                        />
+                        {/*<Tooltip*/}
+                        {/*  content={({ active, payload }) => {*/}
+                        {/*    if (active && payload && payload.length) {*/}
+                        {/*      const { overallRating, potential } =*/}
+                        {/*        payload[0].payload; // 获取当前数据点的能力值和潜力值*/}
+                        {/*      return (*/}
+                        {/*        <div>*/}
+                        {/*          <p>{payload[0].payload.inGameDate}</p>*/}
+                        {/*          <p>*/}
+                        {/*            <b>Potential:</b> {potential}*/}
+                        {/*          </p>*/}
+                        {/*          <p>*/}
+                        {/*            <b>Overall:</b> {overallRating}*/}
+                        {/*          </p>*/}
+                        {/*        </div>*/}
+                        {/*      );*/}
+                        {/*    }*/}
+                        {/*    return null; // 不显示 Tooltip*/}
+                        {/*  }}*/}
+                        {/*/>*/}
+                        <Tooltip />
                         <Area
                           type="monotone"
                           dataKey="potential"
-                          stroke="#000"
-                          strokeWidth={2}
-                          fillOpacity={0.1}
-                          fill="green"
+                          // stroke="#000"
+                          // strokeWidth={2}
+                          // fillOpacity={0.6}
+                          fill="#ffc658"
+                          stroke="#ffc658"
                         ></Area>
                         <Area
                           type="monotone"
                           dataKey="overallRating"
                           stroke="#000"
-                          strokeWidth={2}
-                          fillOpacity={0.8}
-                          fill="green"
+                          strokeWidth={1}
+                          // fillOpacity={0.6}
+                          fill="#82ca9d"
                         ></Area>
                       </AreaChart>
                     </Space>
