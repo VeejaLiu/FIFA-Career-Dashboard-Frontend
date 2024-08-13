94bded514bbd8e8445f5d746d36b5e84cfb4e2a8
fengjun <879818327@qq.com>
2024-08-13 22:31:05 +0800

    fix: 增加代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888',
-        changeOrigin: true,
+
+export default async ({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_BACKEND_URL as string,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [react()],
-});
+    plugins: [react()],
+  });
+};
