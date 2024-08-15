53213eee8740600c7f9f6c16e370955eadda8e66
VeejaLiu <veejaliu@gmail.com>
2024-08-15 19:00:32 +0800

    Add get started page

---
+++ src/constant/user-script.ts
@@ -0,0 +1,239 @@
+export const luaScript = `require 'imports/career_mode/helpers'
+require 'imports/other/helpers'
+local json = require("imports/external/json")
+
+local pre_cm_event_handlers = GetEventHandlers("pre__CareerModeEvent")
+local post_cm_event_handlers = GetEventHandlers("post__CareerModeEvent")
+
+print(string.format("You have %d pre__CareerModeEvents and %d post__CareerModeEvents", #pre_cm_event_handlers, #post_cm_event_handlers))
+
+if #pre_cm_event_handlers > 0 then
+    RemoveEventHandler("pre__CareerModeEvent", pre_cm_event_handlers[1].id)
+end
+
+if #post_cm_event_handlers > 0 then
+    RemoveEventHandler("post__CareerModeEvent", post_cm_event_handlers[1].id)
+end
+
+pre_cm_event_handlers = GetEventHandlers("pre__CareerModeEvent")
+post_cm_event_handlers = GetEventHandlers("post__CareerModeEvent")
+
+print(string.format("You have %d pre__CareerModeEvents and %d post__CareerModeEvents", #pre_cm_event_handlers, #post_cm_event_handlers))
+
+local currentDate = GetCurrentDate()
+
+local systemDateYear = currentDate.year
+local systemDateMonth = currentDate.month
+local systemDateDay = currentDate.day
+
+local currentDateYear = currentDate.year
+local currentDateMonth = currentDate.month
+local currentDateDay = currentDate.day
+print(string.format("Init Current Date: %d-%d-%d", currentDateYear, currentDateMonth, currentDateDay))
+
+function addOneDay()
+    local time_table = {
+        year = currentDateYear,
+        month = currentDateMonth,
+        day = currentDateDay,
+        hour = 0,
+        min = 0,
+        sec = 0
+    }
+
+    local timestamp = os.time(time_table)
+
+    local new_timestamp = timestamp + 24 * 60 * 60
+
+    local new_time_table = os.date("*t", new_timestamp)
+    y, m, d = new_time_table.year, new_time_table.month, new_time_table.day
+    currentDateYear = y
+    currentDateMonth = m
+    currentDateDay = d
+end
+
+function updateDateWhenDayPass()
+    local currentDate = GetCurrentDate()
+    Log(string.format("[updateDateWhenDayPass] Get from GetCurrentDate function: %d-%d-%d", currentDate.year, currentDate.month, currentDate.day))
+    Log(string.format("[updateDateWhenDayPass] Get from systemDate: %d-%d-%d", systemDateYear, systemDateMonth, systemDateDay))
+    Log(string.format("[updateDateWhenDayPass] Get from our currentDate: %d-%d-%d", currentDateYear, currentDateMonth, currentDateDay))
+    if (currentDate.year == systemDateYear
+            and currentDate.month == systemDateMonth
+            and currentDate.day == systemDateDay
+    ) then
+        Log("[updateDateWhenDayPass] System Date is not updated, add one day")
+        addOneDay()
+        local dateStr = string.format("%d-%d-%d", currentDateYear, currentDateMonth, currentDateDay)
+        print(string.format("[updateDateWhenDayPass] Final Current Date: %s", dateStr))
+        return
+    end
+
+    Log("System Date is updated, so we can use it directly")
+    systemDateYear = currentDate.year
+    systemDateMonth = currentDate.month
+    systemDateDay = currentDate.day
+    currentDateYear = currentDate.year
+    currentDateMonth = currentDate.month
+    currentDateDay = currentDate.day
+
+    local dateStr = string.format("%d-%d-%d", currentDate.year, currentDate.month, currentDate.day)
+    Log(string.format("[updateDateWhenDayPass] Final Current Date: %s", dateStr))
+end
+
+function OnDayPassedEvent(events_manager, event_id, event)
+    if (
+            event_id == ENUM_CM_EVENT_MSG_DAY_PASSED
+    ) then
+        updateDateWhenDayPass()
+    end
+end
+
+AddEventHandler("post__CareerModeEvent", OnDayPassedEvent)
+
+local attributeNameList = {
+    "birthdate",
+    "overallrating",
+    "potential",
+    "nationality",
+    "height",
+    "weight",
+    "preferredfoot",
+    "preferredposition1",
+    "preferredposition2",
+    "preferredposition3",
+    "preferredposition4",
+    "skillmoves",
+    "weakfootabilitytypecode",
+    "attackingworkrate",
+    "defensiveworkrate",
+    "acceleration",
+    "sprintspeed",
+    "positioning",
+    "finishing",
+    "shotpower",
+    "longshots",
+    "volleys",
+    "penalties",
+    "vision",
+    "crossing",
+    "freekickaccuracy",
+    "shortpassing",
+    "longpassing",
+    "curve",
+    "agility",
+    "balance",
+    "reactions",
+    "ballcontrol",
+    "dribbling",
+    "composure",
+    "interceptions",
+    "headingaccuracy",
+    "defensiveawareness",
+    "standingtackle",
+    "slidingtackle",
+    "jumping",
+    "stamina",
+    "strength",
+    "aggression",
+    "gkdiving",
+    "gkhandling",
+    "gkkicking",
+    "gkpositioning",
+    "gkreflexes",
+}
+
+function GetUserSeniorTeamPlayerIDs()
+    local result = {}
+    local userTeamID = GetUserTeamID()
+    Log(string.format("User Team ID: %d", userTeamID))
+
+    local careerPlayerContractTable = LE.db:GetTable("career_playercontract")
+    local current_record = careerPlayerContractTable:GetFirstRecord()
+    local c = 1
+    while current_record > 0 do
+        local teamID = careerPlayerContractTable:GetRecordFieldValue(current_record, "teamid")
+        if teamID == userTeamID then
+            local playerID = careerPlayerContractTable:GetRecordFieldValue(current_record, "playerid")
+            result[playerID] = true
+            Log(string.format("%d: %d", c, playerID))
+            c = c + 1
+        end
+        current_record = careerPlayerContractTable:GetNextValidRecord()
+    end
+
+    return result
+end
+
+function postPlayers(jsonStr, dateStr)
+    Log('[postPlayers] Start')
+    local command = 'curl -X POST -H "Content-Type: application/json"'
+    command = command .. ' -H "secret-key: {{user-secret-key}}"'
+
+    local fileName = "fifa_career_dashboard_players-" .. dateStr .. ".json"
+    local file = io.open(fileName, "w")
+    file:write(jsonStr)
+    file:close()
+    command = command .. ' -d "@' .. fileName .. '"'
+
+    command = command .. ' ' .. "https://fifa.009988776.xyz/api/v1/player/bulk"
+
+    Log('[postPlayers] Command: ' .. command)
+
+    os.execute(command)
+    Log('[postPlayers] Done')
+
+end
+
+function sendTeamPlayerAttr()
+    local bIsInCM = IsInCM()
+    if not bIsInCM then
+        return
+    end
+    local dateStr = string.format("%d-%d-%d", currentDateYear, currentDateMonth, currentDateDay)
+    Log(string.format("[sendTeamPlayerAttr] Current Date: %s", dateStr))
+    local userTeamPlayerIDs = GetUserSeniorTeamPlayerIDs()
+    local players_count = table_count(userTeamPlayerIDs)
+    local updated_players = 0
+    local players_table = LE.db:GetTable("players")
+    local current_record = players_table:GetFirstRecord()
+    local jsonStr = ""
+    jsonStr = jsonStr .. "["
+    local playerID = 0
+    while current_record > 0 do
+        playerID = players_table:GetRecordFieldValue(current_record, "playerid")
+        if userTeamPlayerIDs[playerID] then
+            local currentPlayerJsonStr = ""
+            currentPlayerJsonStr = currentPlayerJsonStr .. "{"
+            currentPlayerJsonStr = currentPlayerJsonStr .. string.format('"playerID": %d', playerID)
+            local playerName = GetPlayerName(playerID)
+            currentPlayerJsonStr = currentPlayerJsonStr .. string.format(', "playerName": "%s"', playerName)
+            currentPlayerJsonStr = currentPlayerJsonStr .. string.format(', "currentDate": "%s"', dateStr)
+            for i, attrName in ipairs(attributeNameList) do
+                local attrValue = players_table:GetRecordFieldValue(current_record, attrName)
+                currentPlayerJsonStr = currentPlayerJsonStr .. string.format(', "%s": "%s"', attrName, attrValue)
+            end
+            currentPlayerJsonStr = currentPlayerJsonStr .. "}"
+            updated_players = updated_players + 1
+            jsonStr = jsonStr .. currentPlayerJsonStr
+            if (updated_players < players_count) then
+                jsonStr = jsonStr .. ","
+            end
+        end
+        if (updated_players == players_count) then
+            jsonStr = jsonStr .. "]"
+            postPlayers(jsonStr, dateStr)
+            return
+        end
+        current_record = players_table:GetNextValidRecord()
+    end
+end
+
+function OnEvent(events_manager, event_id, event)
+    if (event_id == ENUM_CM_EVENT_MSG_WEEK_PASSED) then
+        sendTeamPlayerAttr()
+    end
+end
+
+sendTeamPlayerAttr()
+AddEventHandler("post__CareerModeEvent", OnEvent)
+`;
--- src/pages/GetStartedPage.tsx
+++ src/pages/GetStartedPage.tsx
@@ -1,33 +1,125 @@
 import * as React from 'react';
-import { useEffect } from 'react';
-import { Space } from '@douyinfe/semi-ui';
+import { useEffect, useState } from 'react';
+import { Button, CodeHighlight, Notification, Space } from '@douyinfe/semi-ui';
 import { UserApis } from '../service/UserApis.ts';
 import { Typography } from '@douyinfe/semi-ui';
+import { luaScript } from '../constant/user-script.ts';
 
-const { Text, Paragraph } = Typography;
+const { Title, Text, Paragraph } = Typography;
 
 function SettingsPage(): React.ReactElement {
-  const [secretKey, setSecretKey] = React.useState('');
+  const [isCodeExpanded, setIsCodeExpanded] = useState(false);
+  const [codeStr, setCodeStr] = React.useState(luaScript);
 
-  async function fetchSecretKey() {
+  async function getLuaCode() {
     const key = await UserApis.getSecretKey();
     console.log(`[fetchSecretKey] key: ${key}`);
-    setSecretKey(key);
+    setCodeStr(luaScript.replace('{{user-secret-key}}', key));
   }
 
   useEffect(() => {
-    fetchSecretKey().then();
+    getLuaCode().then();
   }, []);
 
   return (
     <Space vertical style={{ padding: '10px' }} align={'start'}>
-      <h1>Get started</h1>
       <Paragraph>
-        asdasfdasdf
-        <Text mark>asdasfdasdf</Text>
-        <Text code>{secretKey}</Text>
-        asdasfdasdf
+        {/*<h1>Get started</h1>*/}
+        <Title heading={1}>Get started</Title>
+
+        {/*<h1>## Dependencies</h1>*/}
+        <Title heading={2}>1. Dependencies</Title>
+        <p>
+          - Latest <b>xAranaktu/FC-24-Live-Editor</b>. (
+          <Text
+            link={{
+              href: 'https://www.patreon.com/collection/96422?view=expanded',
+            }}
+          >
+            Click here{' '}
+          </Text>
+          to download the latest version of Live Editor.)
+        </p>
+
+        <p>- Available internet connection to our backend server.</p>
+        <p>
+          - <b>Secret API key.</b> (You can manage it from
+          <Text
+            link={{
+              href: '/settings',
+            }}
+          >
+            &nbsp;Setting Page
+          </Text>
+          , BUT you don't really need it because I have put it in the code
+          snippet below for you)
+        </p>
+        <p>
+          - <b>Lua code snippet </b>(You can copy the code snippet below and the
+          secret key will be automatically replaced)
+        </p>
+
+        {/*<h1>## Installation</h1>*/}
+        <Title heading={2}>2. Installation</Title>
+        <p>- Open the FC24 with Live Editor.</p>
+        <p>- Go to the `Lua script` tab in the live editor.</p>
+        <p>- Paste the code snippet below.</p>
       </Paragraph>
+      <Space>
+        <Button
+          onClick={() => {
+            navigator.clipboard
+              .writeText(codeStr)
+              .then((r) => {
+                console.log('Copied');
+                Notification.success({
+                  position: 'topRight',
+                  title: 'Success',
+                  content: 'Copied to clipboard',
+                  duration: 3,
+                });
+              })
+              .catch((e) => {
+                Notification.error({
+                  position: 'topRight',
+                  title: 'Error',
+                  content: 'Failed to copy. Please manually copy the code.',
+                  duration: 3,
+                });
+              });
+          }}
+        >
+          Copy to clipboard
+        </Button>
+
+        <Button onClick={() => setIsCodeExpanded(!isCodeExpanded)}>
+          {isCodeExpanded ? '↑↑↑ Hide all code' : '↓↓↓ Show all code'}
+        </Button>
+      </Space>
+
+      <Text type="warning">
+        Warning: These codes contain your secret key. Do not share these codes
+        with others.
+      </Text>
+      <div
+        style={{
+          maxHeight: isCodeExpanded ? 'none' : '150px',
+          overflow: 'scroll',
+          transition: 'max-height 0.3s ease',
+          border: '1px solid #ddd',
+          padding: '10px',
+          position: 'relative',
+        }}
+      >
+        <CodeHighlight
+          code={codeStr}
+          language="JavaScript"
+          className={'code-highlight'}
+          defaultTheme={true}
+          lineNumber={false}
+          style={{}}
+        ></CodeHighlight>
+      </div>
     </Space>
   );
 }
