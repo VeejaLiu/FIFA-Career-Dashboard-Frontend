0a57f3380036f4db8b586a03b1f3a89cd79e841f
Veeja Liu <veejaliu@gmail.com>
2024-09-01 22:31:59 +0800

    Change the notification style and refactor the code

--- src/components/WebsocketNotification.tsx
+++ src/components/WebsocketNotification.tsx
@@ -4,6 +4,75 @@ import { Notification, Space } from '@douyinfe/semi-ui';
 let reconnectInterval = 1000; // 初始重连间隔
 const maxReconnectInterval = 30000; // 最大重连间隔
 
+function getColorByDiff(diff: number) {
+  switch (true) {
+    case diff > 0:
+      return '#2ef72e';
+    case diff < 0:
+      return '#dc3545';
+    default:
+      return '#999';
+  }
+}
+
+/**
+ * Send notification when player overall rating updated
+ * @param payload
+ */
+function overratingChangeNotification(payload: any) {
+  const {
+    playerID,
+    playerName,
+    oldOverallrating,
+    overallrating,
+    oldPotential,
+    potential,
+  } = payload;
+  Notification.success({
+    title: 'Player Update',
+    // content: `Player ${playerName}[${playerID}] updated: overallrating ${oldOverallrating} -> ${overallrating}, potential ${oldPotential} -> ${potential}`,
+    content: (
+      <Space vertical align={'start'}>
+        <div>
+          <span style={{ fontWeight: 'bold' }}> {playerName}</span>
+          <span style={{ color: '#999' }}> [ID: {playerID}]</span>
+        </div>
+        <div>
+          <span style={{ marginRight: '8px' }}>Overall rating: </span>
+          <span style={{ marginRight: '8px', color: '#999' }}>
+            {oldOverallrating}
+          </span>
+          <span style={{ marginRight: '8px' }}>{'->'}</span>
+          <span
+            style={{
+              fontWeight: 'bold',
+              color: getColorByDiff(overallrating - oldOverallrating),
+            }}
+          >
+            {overallrating}
+          </span>
+        </div>
+        <div>
+          <span style={{ marginRight: '8px' }}>Potential: </span>
+          <span style={{ marginRight: '8px', color: '#999' }}>
+            {oldPotential}
+          </span>
+          <span style={{ marginRight: '8px' }}>{'->'}</span>
+          <span
+            style={{
+              fontWeight: 'bold',
+              color: getColorByDiff(potential - oldPotential),
+            }}
+          >
+            {potential}
+          </span>
+        </div>
+      </Space>
+    ),
+    duration: 30000,
+  });
+}
+
 export const WebsocketNotification = () => {
   async function createWebSocketConnection() {
     const token = localStorage.getItem('fcd-token');
@@ -66,70 +135,7 @@ export const WebsocketNotification = () => {
       console.log('[WebSocket][onmessage] type:', type, 'payload:', payload);
       switch (type) {
         case 'PlayerUpdate.Overall': {
-          const {
-            playerID,
-            playerName,
-            oldOverallrating,
-            overallrating,
-            oldPotential,
-            potential,
-          } = payload;
-          Notification.success({
-            title: 'Player Update',
-            // content: `Player ${playerName}[${playerID}] updated: overallrating ${oldOverallrating} -> ${overallrating}, potential ${oldPotential} -> ${potential}`,
-            content: (
-              <Space vertical align={'start'}>
-                <div>
-                  <span style={{ fontWeight: 'bold' }}> {playerName}</span>
-                  <span style={{ color: '#999' }}> [ID: {playerID}]</span>
-                </div>
-                <div>
-                  <span style={{ marginRight: '8px' }}>Overall rating: </span>
-                  <span
-                    style={{
-                      marginRight: '8px',
-                      color: '#999',
-                    }}
-                  >
-                    {oldOverallrating}
-                  </span>
-                  <span style={{ marginRight: '8px' }}>{'->'}</span>
-                  <span
-                    style={{
-                      fontWeight: 'bold',
-                      color:
-                        overallrating > oldOverallrating
-                          ? '#2ef72e'
-                          : '#dc3545',
-                    }}
-                  >
-                    {overallrating}
-                  </span>
-                </div>
-                <div>
-                  <span style={{ marginRight: '8px' }}>Potential: </span>
-                  <span
-                    style={{
-                      marginRight: '8px',
-                      color: '#999',
-                    }}
-                  >
-                    {oldPotential}
-                  </span>
-                  <span style={{ marginRight: '8px' }}>{'->'}</span>
-                  <span
-                    style={{
-                      fontWeight: 'bold',
-                      color: potential > oldPotential ? '#2ef72e' : '#dc3545',
-                    }}
-                  >
-                    {potential}
-                  </span>
-                </div>
-              </Space>
-            ),
-            duration: 30000,
-          });
+          overratingChangeNotification(payload);
           break;
         }
         default:
