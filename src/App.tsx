c54dca2a7fc61146fe2419999b31a9722c95f29a
Veeja Liu <veejaliu@gmail.com>
2024-10-14 20:14:08 +0800

    feat: Add cursor pointer to WebsiteLogoComponent

--- src/App.tsx
+++ src/App.tsx
@@ -112,6 +112,7 @@ function WebsiteLogoComponent() {
           style={{
             color: 'gray',
             fontSize: '0.8rem',
+            cursor: 'pointer',
           }}
           underline
         >
