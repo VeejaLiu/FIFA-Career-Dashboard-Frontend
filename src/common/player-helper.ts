7b529e83c69810396edd781287340e29993b11e0
VeejaLiu <veejaliu@gmail.com>
2024-10-30 10:44:41 +0800

    Fixed the avatar for FC25

--- src/common/player-helper.ts
+++ src/common/player-helper.ts
@@ -1,3 +1,5 @@
+import { getDefaultGameVersion } from './common.ts';
+
 export function getColorByPositionType(positionType: string) {
   switch (positionType) {
     case 'FOR':
@@ -32,15 +34,21 @@ export function getColorByOverallRating(overallRating: number) {
   }
 }
 
-export function getAvatarUrl(playerID: number | null | undefined) {
+export function getAvatarUrl(playerID: number | null | undefined): string {
+  const gameVersion = getDefaultGameVersion();
+
   if (!playerID) {
     return '';
   }
   // playerID example: 158023
-  // src example: https://cdn.sofifa.net/players/158/023/24_120.png
+  // src example: https://cdn.sofifa.net/players/158/023/24_120.png for FIFA 24
+  // src example: https://cdn.sofifa.net/players/158/023/25_120.png for FIFA 25
+
   const a = playerID.toString().slice(0, 3);
   const b = playerID.toString().slice(3, 6);
-  const imageUrl = `https://cdn.sofifa.net/players/${a}/${b}/24_120.png`;
+
+  const imageUrl = `https://cdn.sofifa.net/players/${a}/${b}/${gameVersion}_120.png`;
+
   return imageUrl;
 }
 
